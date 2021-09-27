8b64c28e0245fa70d6cbe050ce7b460316e0b0ca
Lost Illusion <im@lostillusion.net>
2021-09-26 21:43:29 -0400

    add nonstandard resolution

--- vgasrc/svgamodes.c
+++ vgasrc/svgamodes.c
@@ -92,5 +92,10 @@ struct generic_svga_mode svga_modes[] VAR16 = {
     { 0x196, { MM_DIRECT, 2560, 1440, 16, 8, 16, SEG_GRAPH } },
     { 0x197, { MM_DIRECT, 2560, 1440, 24, 8, 16, SEG_GRAPH } },
     { 0x198, { MM_DIRECT, 2560, 1440, 32, 8, 16, SEG_GRAPH } },
+
+    /* custom resolutions for non-standard displays */
+    { 0x199, { MM_DIRECT, 1366,  768, 16, 8, 16, SEG_GRAPH } },
+    { 0x200, { MM_DIRECT, 1366,  768, 24, 8, 16, SEG_GRAPH } },
+    { 0x201, { MM_DIRECT, 1366,  768, 32, 8, 16, SEG_GRAPH } },
 };
 unsigned int svga_mcount VAR16 = ARRAY_SIZE(svga_modes);
